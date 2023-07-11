bf996b031453598a627b4a916b0a8f6e3c3ce96f
PedroVidal2703 <pedrovidal2703@gmail.com>
2023-07-11 13:32:15 -0300

    feat: register commands globally

--- src/register-commands.js
+++ src/register-commands.js
@@ -25,7 +25,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 (async () => {
 	try {
 		console.log("Registering commands...");
-		await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
+		await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
 		console.log("Commands Registered!");
 	} catch (error) {
 		console.error(`There was an error: ${error}`);
