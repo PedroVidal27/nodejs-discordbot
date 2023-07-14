9436017267d81695aa9775356ab59ef7915b7348
PedroVidal2703 <pedrovidal2703@gmail.com>
2023-07-14 13:55:39 -0300

    fix: bot is finally working again

--- src/music-player.js
+++ src/music-player.js
@@ -33,36 +33,14 @@ class MusicPlayer {
 		const guildId = client.guildId;
 		const playerIndex = this.playerList.findIndex((player) => player.guildId === guildId);
 		if (playerIndex != -1) {
-			const player = this.playerList[playerIndex].player;
-			const guild = await client.guilds.fetch(guildId);
-			console.log(guild);
-			const guildMember = await guild.members.fetch(interaction.user.id);
-			const connection = joinVoiceChannel({
-				channelId: guildMember.voice.channelId,
-				guildId: guildId,
-				adapterCreator: guild.voiceAdapterCreator
-			});
-			const stream = await ytdl(interaction.options.get("youtube-link").value, { filter: "audioonly" });
-			const resource = createAudioResource(stream, { seek: 0, volume: 1 });
-			player.play(resource);
-			player.on("error", (error) => {
-				console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
-				player.play(getNextResource());
-			});
-			connection.subscribe(player);
-
-			player.on(AudioPlayerStatus.Playing, () => {
-				console.log("Playing!");
-				return;
-			});
-			player.on(AudioPlayerStatus.Idle, async () => {
+			const player = this.playerList[playerIndex];
+			console.log(player.state.status);
+			if(player.state.status === 'idle'){
 				console.log("Not playing");
-				const guild = await client.guilds.fetch(interaction.guildId);
-				const guildMember = await guild.members.fetch(interaction.user.id);
 				const connection = joinVoiceChannel({
-					channelId: guildMember.voice.channelId,
-					guildId: guildId,
-					adapterCreator: guild.voiceAdapterCreator
+					channelId: interaction.member.voice.channel.id,
+					guildId: interaction.guildId,
+					adapterCreator: interaction.guild.voiceAdapterCreator
 				});
 				const stream = await ytdl(interaction.options.get("youtube-link").value, { filter: "audioonly" });
 				const resource = createAudioResource(stream, { seek: 0, volume: 1 });
@@ -72,7 +50,8 @@ class MusicPlayer {
 					player.play(getNextResource());
 				});
 				connection.subscribe(player);
-			});
+				return;
+			};
 			return;
 		}
 		this.newPlayer(guildId);
